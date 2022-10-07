3da7c86ab96fdd190bd6d3c23da24f57588eb1dc
Amirul Faiz <faizshamsul98@gmail.com>
2022-10-08 02:54:21 +0800

    Working with DB

---
+++ database/seeders/ScheduleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ScheduleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schedule::create([
+            'title' => 'Training',
+            'description' => 'Training Laravel',
+        ]);
+    }
+}
