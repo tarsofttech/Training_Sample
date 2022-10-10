a2aea7a2e9097feba4c1f392e28cf30d9c129d07
Tarmizi Sanusi <realtarmizisanusi@gmail.com>
2022-10-10 22:46:18 +0800

    feat: add crud cont

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::get('/schedules', [App\Http\Controllers\ScheduleController::class, 'index
 Route::get('/schedules/create', [App\Http\Controllers\ScheduleController::class, 'create'])->name('schedule.create');
 Route::post('/schedules/create', [App\Http\Controllers\ScheduleController::class, 'store'])->name('schedule.store');
 Route::get('/schedules/{schedule}', [App\Http\Controllers\ScheduleController::class, 'show'])->name('schedule.show');
+Route::get('/schedules/{schedule}/edit', [App\Http\Controllers\ScheduleController::class, 'edit'])->name('schedule.edit');
+Route::post('/schedules/{schedule}/edit', [App\Http\Controllers\ScheduleController::class, 'update'])->name('schedule.update');
+Route::get('/schedules/{schedule}/delete', [App\Http\Controllers\ScheduleController::class, 'delete'])->name('schedule.delete');
+
