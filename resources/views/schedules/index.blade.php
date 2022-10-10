24a1d5e5c407a157168702f6b440e0aadaf7877c
Tarmizi Sanusi <realtarmizisanusi@gmail.com>
2022-10-10 23:06:33 +0800

    chore: ui tweaks

--- resources/views/schedules/index.blade.php
+++ resources/views/schedules/index.blade.php
@@ -9,25 +9,29 @@
 
                 <div class="card-body">
                     <table class="table">
-                        <tr>
-                            <th>No.</th>
-                            <th>Title</th>
-                            <th>Description</th>
-                            <th>Created</th>
-                            <th>Action</th>
-                        </tr>
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">No.</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Created</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
                         @foreach ($schedules as $schedule)
-                        <tr>
-                            <td>{{ $schedule->id }}</td>
-                            <td>{{ $schedule->title }}</td>
-                            <td>{{ $schedule->description }}</td>
-                            <td>{{ $schedule->created_at->diffForHumans() }}</td>
-                            <td>
-                                <a href="{{ route('schedule.show', $schedule) }}" class="btn btn-primary">Show</a>
-                                <a href="{{ route('schedule.edit', $schedule) }}" class="btn btn-success">Edit</a>
-                                <a href="{{ route('schedule.delete', $schedule) }}" class="btn btn-danger">Delete</a>
-                            </td>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td>{{ $schedule->id }}</td>
+                                <td>{{ $schedule->title }}</td>
+                                <td>{{ $schedule->description }}</td>
+                                <td>{{ $schedule->created_at->diffForHumans() }}</td>
+                                <td>
+                                    <a href="{{ route('schedule.show', $schedule) }}" class="btn btn-primary">Show</a>
+                                    <a href="{{ route('schedule.edit', $schedule) }}" class="btn btn-success">Edit</a>
+                                    <a onclick="return confirm('Are you sure?')" href="{{ route('schedule.delete', $schedule) }}" class="btn btn-danger">Delete</a>
+                                </td>
+                            </tr>
+                        </tbody>
                         @endforeach
                     </table>
                 </div>
